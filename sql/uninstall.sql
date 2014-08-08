50efd72b3b0ae70b7c99007388bb8e192cefbd0e
Robert Leonhardt <root@ubuntu.itz.uni-halle.de>
2014-08-08 12:00:41 +0200

    another renaming of OC object property

--- sql/uninstall.sql
+++ sql/uninstall.sql
@@ -1,4 +1,4 @@
 DROP TABLE IF EXISTS `oc_series`,`oc_config`,`oc_endpoints`, `oc_seminar_series`, `oc_resources`, `oc_seminar_episodes`, `oc_scheduled_recordings`;
 DELETE FROM  resources_objects_properties 
-WHERE property_id IN(SELECT property_id FROM resources_properties WHERE name = 'System zur automatischen Vorlesungsaufzeichnung' );
-DELETE FROM resources_properties WHERE name = 'System zur automatischen Vorlesungsaufzeichnung';
+WHERE property_id IN(SELECT property_id FROM resources_properties WHERE name = 'automatische Vorlesungsaufzeichnung' );
+DELETE FROM resources_properties WHERE name = 'automatische Vorlesungsaufzeichnung';
